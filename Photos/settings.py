83b0de72bc64e970bfb42520b021b4f8d56f5154
Nasir-gaayte <slowboyz12@gmail.com>
2022-11-10 22:08:42 +0300

    add account app

--- Photos/settings.py
+++ Photos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'acc',
     'django_bootstrap5',
 ]
 
@@ -128,3 +129,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login/'
--- Photos/urls.py
+++ Photos/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
+    path('',include('acc.urls')),
 ]
 
 
